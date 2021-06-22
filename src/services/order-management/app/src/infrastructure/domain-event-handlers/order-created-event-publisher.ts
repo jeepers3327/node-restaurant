19256bccffe166684425c7819643e7a5fb6175ef
James Eastham <j.eastham1993@gmail.com>
2021-06-22 21:32:27 +0100

    Add domain event handler with simple implementation

---
+++ src/services/order-management/app/src/infrastructure/domain-event-handlers/order-created-event-publisher.ts
@@ -0,0 +1,9 @@
+import { IHandler } from "node-js-ddd/dist/events/domain-event-handling";
+import { OrderCreatedEvent } from "../../domain/events/order-created/order-created";
+
+export class OrderCreatedEventPublisher implements IHandler<OrderCreatedEvent>{
+    typeToHandle = 'order-created';
+    handle(evt: OrderCreatedEvent): void {
+        console.log(`Event handling running for ${evt}`);
+    }
+}
