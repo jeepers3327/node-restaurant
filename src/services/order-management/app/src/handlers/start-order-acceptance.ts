75436db4e8eca1e90eb8fde24b800f22358c6111
James Eastham <dev@jameseastham.co.uk>
2021-07-02 22:10:17 +0100

    Fix file naming issue

--- src/services/order-management/app/src/handlers/start-order-acceptance.ts
+++ src/services/order-management/app/src/handlers/start-order-acceptance.ts
@@ -1,13 +1,7 @@
 import { OrderRepositoryFaunaDbImpl } from '../infrastructure/order-repository-fauna-db';
 import { OrderAcceptanceState } from '../infrastructure/state-machine/order-acceptance-state';
-import { WinstonLogger } from '../infrastructure/logger-winston';
 
-const logger = new WinstonLogger();
-
-export const handler = async (evt) => {
-  logger.logInformation('Received event');
-  logger.logInformation(evt.orderNumber);
-  
+export const handler = async (evt) => {  
   const orders = new OrderRepositoryFaunaDbImpl(process.env.FAUNA_DB_ACCESS_KEY);
 
   const state = new OrderAcceptanceState(await orders.getSpecific(evt.customerId, evt.orderNumber));
